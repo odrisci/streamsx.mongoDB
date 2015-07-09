bd8e6df919f041a6505ebc427d07b851453ab288
Cillian O'Driscoll <cillian.odriscoll@gmail.com>
2015-07-09 16:50:50 +0100

    Updated setLinks.sh to work outside streamsStudio

--- com.ibm.streamsx.mongodb/impl/bin/setLinks.sh
+++ com.ibm.streamsx.mongodb/impl/bin/setLinks.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-MONGODB_TOOLKIT=$(echo ${STREAMS_STUDIO_SPL_PATH} | grep -oP '/[^:]*com.ibm.streamsx.mongodb')
+CURR_DIR=$(dirname $(readlink -f $0))
+MONGODB_TOOLKIT=${CURR_DIR}/../..
 
 if [[ ! -L $MONGODB_TOOLKIT/impl/include/boost ]]; then
 	ln -s $STREAMS_INSTALL/ext/include/streams_boost $MONGODB_TOOLKIT/impl/include/boost
